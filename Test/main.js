00660f5a266f69f1faa273a5f13f1af41883a66a
miguel-990624 <miguelangelmolinagutierrez@gmail.com>
2025-07-14 10:26:02 -0500

    second mental breakdown

--- Test/main.js
+++ Test/main.js
@@ -5,7 +5,7 @@ const routes = {
     "/": "/html/login.html",
     "/events": "/html/events.html",
     "/add": "/html/add.html",
-    "/cart": "/html/cart.html",
+    "/enrollment": "/html/enrollment.html",
     "/edit": "/html/edit.html",
     "/register": "/html/register.html"
 };
@@ -20,16 +20,40 @@ document.body.addEventListener("click", (e) => {
 });
 
 async function navigate(path) {
-
+    //const currentUser = localStorage.getItem("currentUser").JSON.parse();
     const isLoggedIn = localStorage.getItem("loggedIn") === "true";
     const isAdmin = localStorage.getItem("isAdmin");
+    const userRole = localStorage.getItem("role");
     const navbar = document.getElementById("navbar");
     const AdminNavbar =
         `
+        <h2>Events</h2>
+        <section>
+            <img src="./resources/profile.png" alt="profile" class="profile-img">
+            <div class="profile-info">
+                <h3>${localStorage.getItem("name")}</h3>
+                <p>${userRole}</p>
+            </div>
+        </section>
     <nav>
         <a href="/events" data-link>Events</a>
-        <a href="/add" data-link>Add New Product</a>
-        <a href="/cart" data-link>Cart</a>
+        
+        <button id="logout-btn" type="button">Logout</button>
+    </nav>
+    `;
+
+    const UserNavbar =
+    `
+    <h2>Events</h2>
+        <section>
+            <img src="./resources/profile.png" alt="profile" class="profile-img">
+            <div class="profile-info">
+                <h3>${localStorage.getItem("name")}</h3>
+                <p>${userRole}</p>
+            </div>
+        </section>
+    <nav>
+        <a href="/events" data-link>Events</a>
         <button id="logout-btn" type="button">Logout</button>
     </nav>
     `;
@@ -48,11 +72,11 @@ async function navigate(path) {
         navbar.innerHTML = "";
     }
 
-    if (!isLoggedIn && path !== "/") {
+    if (!isLoggedIn && path !== "/" && path !== "/register") {
         return navigate("/");
     }
 
-    if (isLoggedIn && path === "/") {
+    if (isLoggedIn && path === "/" && path === "/register") {
         return navigate("/events");
     }
 
@@ -70,7 +94,7 @@ async function navigate(path) {
     if (path.startsWith("/edit")) {
         setupEditForm(path);
     }
-    if (path === "/cart") setupCart();
+    // if (path === "/enrollment") setupCart();
     history.pushState({}, "", path);
 };
 
@@ -91,6 +115,11 @@ function setupLogin() {
             const user = users.find((u) => u.email === loginEmail && u.password === loginPassword);
 
               if (user) {
+                localStorage.setItem("name" , user.name);
+                localStorage.setItem("email", user.email);
+                localStorage.setItem("userID", user.id);
+                localStorage.setItem("role", user.role);
+                localStorage.setItem("ID", user.id);
                 localStorage.setItem("loggedIn", "true");
                 localStorage.setItem("isAdmin", (user.role === "admin"));
                 navigate("/events");
@@ -151,8 +180,19 @@ function setupRegister() {
 }
 
 function setupEvents() {
+    const addNew = document.getElementById("addNew");
     const display = document.getElementById("event-list");
     const isAdmin = localStorage.getItem("isAdmin");
+
+    if(isAdmin === "true" || isAdmin === true) {
+        addNew.innerHTML = `
+        <button id="add-btn" type="button" class="add-btn">
+            <a href="/add" data-link>ADD NEW EVENT</a>
+        </button>
+        `;
+    } else {
+        addNew.innerHTML = "";
+    }
     display.innerHTML = "";
     fetch("http://localhost:3000/events")
         .then(res => {
@@ -163,7 +203,7 @@ function setupEvents() {
         })
         .then(data => {
             if (data.length === 0) {
-                if( isAdmin) {
+                if( isAdmin === true || isAdmin === "true") {
                     display.innerHTML = "<p>No events available. Please add some.</p>";
                 }else {
                     display.innerHTML = "<p>No events available.</p>";
@@ -173,24 +213,59 @@ function setupEvents() {
             data.forEach(element => {
                 const tableRow = document.createElement("tr");
                 tableRow.innerHTML = `
+                <td><img src="./resources/events.png" alt="event"></td>
                 <td>${element.name}</td>
-                <td>${element.price}</td>
+                <td>${element.description}</td>
+                <td>${element.capacity}</td>
+                <td>${element.date}</td>
                 `;
-                const editBtn = document.createElement("button");
-                editBtn.classList.add("edit-btn");
-                editBtn.textContent = "Edit";
-                editBtn.addEventListener("click", () => {
-                    navigate(`/edit?id=${element.id}`)
-                });
 
-                const addBtn = document.createElement("button");
-                addBtn.textContent = "Add to Cart";
-                addBtn.addEventListener("click", () => {
-                    addToCart(element);
-                });
+                const buttonContainer = document.createElement("td");
 
-                tableRow.appendChild(editBtn);
-                tableRow.appendChild(addBtn);
+                if (isAdmin === "true" || isAdmin === true) {
+
+                    const editBtn = document.createElement("button");
+                    editBtn.classList.add("edit-btn");
+                    editBtn.innerHTML = "<img src='./resources/edit.png' alt='edit' class='edit-btn'>";
+                    editBtn.addEventListener("click", () => {
+                        navigate(`/edit?id=${element.id}`)
+                    });
+
+                    const deleteBtn = document.createElement("button");
+                    deleteBtn.innerHTML = "<img src='./resources/delete.png' alt='delete' class='delete-btn'>";
+                    deleteBtn.classList.add("delete-btn");
+                    deleteBtn.addEventListener("click", () => {
+                        fetch(`http://localhost:3000/events/${element.id}`,{
+                            method: "DELETE"
+                        })
+                        .then(res => {
+                            if (!res.ok) {
+                                throw new Error("Failed to delete event");
+                            }
+                            return res.json();
+                        })
+                        .then(() => {
+                            alert("Event deleted successfully");
+                            navigate("/events");
+                        })
+                        .catch(error => console.error("Oops, looks like something went wrong", error));
+                    });
+
+                    buttonContainer.appendChild(editBtn);
+                    buttonContainer.appendChild(deleteBtn);
+
+                }else {
+                    const enrollBtn = document.createElement("button");
+                    enrollBtn.classList.add("enroll-btn");
+                    enrollBtn.innerHTML = "enroll";
+                    enrollBtn.addEventListener("click", () => {
+
+                    });
+
+                    buttonContainer.appendChild(enrollBtn);
+                }
+
+                tableRow.appendChild(buttonContainer);
                 display.appendChild(tableRow);
             })
         })
@@ -203,9 +278,12 @@ function setupAddForm() {
         e.preventDefault();
 
         const formName = document.getElementById("name").value.trim();
-        const formPrice = document.getElementById("price").value.trim();
+        const formDescription = document.getElementById("description").value.trim();
+        const formCapacity = document.getElementById("capacity").value.trim();
+        const formDate = document.getElementById("date").value.trim();
+        const eventOcupation = 0;
 
-        if (!formName || !formPrice || isNaN(formPrice)) {
+        if (!formName || !formDescription || !formCapacity || !formDate) {
             alert("Please enter valid values");
             return;
         };
@@ -213,7 +291,13 @@ function setupAddForm() {
         fetch("http://localhost:3000/events", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name: formName, price: Number(formPrice) })
+            body: JSON.stringify({ 
+                name: formName,
+                description: formDescription,
+                capacity: Number(formCapacity),
+                date: formDate,
+                ocuppation: eventOcupation
+            })
         })
             .then(res => {
                 if (!res.ok) {
@@ -229,71 +313,45 @@ function setupAddForm() {
 }
 
 function setupEditForm(path) {
-    const URLparam = new URLSearchParams(path.split("?")[1]);
-    const id = URLparam.get("id");
     const form = document.getElementById("edit-form");
+    
+    let eventID = path.split("?")[1];
+    eventID = eventID.split("=")[1];
 
-    if (!id) {
-        console.error("No product ID found in URL");
-        return;
-    }
-
-    fetch(`http://localhost:3000/events/${id}`)
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById("name").value = data.name;
-            document.getElementById("price").value = data.price;
-        });
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         const updatedName = document.getElementById("name").value.trim();
-        const updatedPrice = document.getElementById("price").value.trim();
+        const updatedDescription = document.getElementById("description").value.trim();
+        const updatedCapacity = document.getElementById("capacity").value.trim();
+        const updatedDate = document.getElementById("date").value.trim();
 
-        fetch(`http://localhost:3000/events/${id}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                name: updatedName,
-                price: Number(updatedPrice)
+        fetch(`http://localhost:3000/events/${eventID}`)
+        .then(res => res.json())
+        .then(data => {
+
+            fetch(`http://localhost:3000/events/${eventID}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    id: eventID,
+                    name: updatedName,
+                    description: updatedDescription,
+                    capacity: Number(updatedCapacity),
+                    date: updatedDate,
+                    ocuppation: data.ocuppation
+                })
             })
-        })
             .then(res => res.json())
             .then(() => {
                 navigate("/events");
             });
+        })
     });
+        
 }
 
-function addToCart(product) {
-    let cart = sessionStorage.getItem("cart");
-    cart = cart ? JSON.parse(cart) : [];
-    cart.push(product);
-    sessionStorage.setItem("cart", JSON.stringify(cart));
-}
 
-function setupCart() {
-    const cartContainer = document.getElementById("cart-container");
-    cartContainer.innerHTML = "";
-
-    let cart = sessionStorage.getItem("cart");
-    cart = cart ? JSON.parse(cart) : [];
-
-    cart.forEach(item => {
-        cartItem = document.createElement("div");
-        cartItem.innerHTML = `
-        <h2>${item.name}</h2>
-        <h3>${item.price}</h3>
-        `;
-        cartContainer.appendChild(cartItem);
-    })
-
-    const total = cart.reduce((sum, item) => sum + Number(item.price), 0);
-
-    const totalDiv = document.createElement("div");
-    totalDiv.innerHTML = `<strong>Total: ${total}</strong>`;
-    cartContainer.appendChild(totalDiv);
-}
 
 window.addEventListener("popstate", () => {
     navigate(location.pathname);
